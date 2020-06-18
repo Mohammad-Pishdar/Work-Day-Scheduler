0a9a3942852f7f16708a1def1964d662ef6afc03
Mo Pishdar <mohpish@gmail.com>
2020-06-18 16:00:37 +1000

    compeletly changed and fixed the time blocks colouring algorithm

--- script.js
+++ script.js
@@ -2,26 +2,26 @@ const currentDayPlaceholder = $("#currentDay");
 const timeInTimeBlocks = $(".input-group-text");
 const timeBlockInput = $(".form-control");
 const saveButton = $(".saveBtn");
-let numericCurrentTime = parseInt(moment().format("h A"));
+let numericCurrentTime = parseInt(moment().format("H A"));
 
 
 currentDayPlaceholder.append(moment().format('dddd, MMMM Do'));
 
 function timeBlocksColorDeterminator() {
+
+    localStorage.getItem("timeBlockNotes");
+
     for (let i = 0; i < timeInTimeBlocks.length; i++) {
 
         let numericTimeinTimeBlock = parseInt($(timeInTimeBlocks[i]).text());
-
-        if (moment().format("h A") === $(timeInTimeBlocks[i]).text()) {
+        if ($(timeInTimeBlocks[i]).hasClass('pm')) {
+            numericTimeinTimeBlock += 12;
+        }
+        if (numericCurrentTime === numericTimeinTimeBlock) {
             $(timeBlockInput[i]).addClass("present");
-        }
-        if (numericCurrentTime > numericTimeinTimeBlock && $(timeInTimeBlocks[i]).hasClass("pm")) {
-            $(timeBlockInput[i]).addClass("future");
-        }
-        if (parseInt(moment().format("h A")) > numericTimeinTimeBlock) {
+        } else if (numericCurrentTime > numericTimeinTimeBlock) {
             $(timeBlockInput[i]).addClass("past");
-        }
-        if (parseInt(moment().format("h A")) < numericTimeinTimeBlock) {
+        } else {
             $(timeBlockInput[i]).addClass("future");
         }
 
